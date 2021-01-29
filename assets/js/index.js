a4b320368ad1159abb9647292972c89d8c79b421
wangjiawei <1874750235@qq.com>
2021-01-29 17:25:45 +0800

    提交了个人中心模块

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,57 +1,52 @@
-$(function() {
-    //从layui中提取模块
-    const { layer } = layui
+  //1.获取用户的个人信息
+  function getUserInfo() {
+
+      //发送ajax请求
+      axios.get('/my/userinfo').then(res => {
+          console.log(res);
+          //校验请求失败
+          if (res.status !== 0) {
+              return layer.msg('获取用户信息失败！')
+          }
+          const { data } = res
+
+          //1.获取用户名
+          const name = data.nickname || data.username;
+          //2.渲染昵称
+          $('.nickname').text(`欢迎${name}`).show();
+          //3.渲染头像
+          if (data.user_pic) {
+              //注意:base64格式的编码字符串可以直接设置给img的src属性
+              $('.avatar').prop('src', data.user_pic).show()
+              $('.text-avatar').hide()
+          } else {
+              $('.text-avatar').text(name[0].toUpperCase()).show()
+              $('.avatar').hide()
+          }
+      })
+
+  }
+
+  getUserInfo()
 
 
-
-
-    //1.获取用户的个人信息
-    function getUserInfo() {
-
-        //发送ajax请求
-        axios.get('/my/userinfo').then(res => {
-            console.log(res);
-            //校验请求失败
-            if (res.status !== 0) {
-                return layer.msg('获取用户信息失败！')
-            }
-            const { data } = res
-
-            //1.获取用户名
-            const name = data.nickname || data.username;
-            //2.渲染昵称
-            $('.nickname').text(`欢迎${name}`).show();
-            //3.渲染头像
-            if (data.user_pic) {
-                //注意:base64格式的编码字符串可以直接设置给img的src属性
-                $('.avatar').prop('src', data.user_pic).show()
-                $('.text-avatar').hide()
-            } else {
-                $('.text-avatar').text(name[0].toUpperCase()).show()
-                $('.avatar').hide()
-            }
-        })
-
-    }
-
-    getUserInfo()
-
-    //2.点击退出
-    $('#logout').click(function() {
-        //请求接口(模拟)
-        //1.清除token
-        localStorage.removeItem('token')
-            //2.跳转到登录页面
-        location.href = './login.html'
-    })
+  $(function() {
+      //从layui中提取模块
+      const { layer } = layui
 
 
 
 
 
 
-
-
+      //2.点击退出
+      $('#logout').click(function() {
+          //请求接口(模拟)
+          //1.清除token
+          localStorage.removeItem('token')
+              //2.跳转到登录页面
+          location.href = './login.html'
+      })
 
 
 
@@ -66,4 +61,12 @@ $(function() {
 
 
 
-})
+
+
+
+
+
+
+
+
+  })
---
+++ assets/js/user/base-info.js
@@ -0,0 +1,89 @@
+// 基本资料功能
+$(function() {
+    const { layer, form } = layui
+    //1.页面一加载获取用户信息
+    function initUserInfo() {
+        axios.get('/my/userinfo').then(res => {
+            //校验请求失败
+            if (res.status !== 0) {
+                return layer.msg('获取失败!')
+            }
+            const { data } = res
+            //给表单赋值
+            //注意edit-userinfo是表单lay-filter属性的值
+            //data对象中的属性名和表单name值一 一对应
+            form.val('edit-userinfo', data)
+        })
+    }
+
+    initUserInfo()
+
+    //2.表单验证
+    form.verify({
+        nick: [
+            /^\S{1,7}$/,
+            '昵称长度必须在1~6个字符之间'
+        ]
+    })
+
+    //3.提交修改
+    $('.base-info-form').submit(function(e) {
+        e.preventDefault()
+
+        //发送ajax请求
+        axios.post('/my/userinfo', $(this).serialize())
+            .then(res => {
+                console.log(res);
+                //校验失败
+                if (res.status !== 0) {
+                    return layer.msg('修改信息失败!')
+                }
+                layer.msg('修改信息成功')
+
+                //更新用户信息
+                window.parent.getUserInfo()
+
+            })
+    })
+
+    //4.重置功能
+    $('#reset-btn').click(function(e) {
+        e.preventDefault()
+        initUserInfo()
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
